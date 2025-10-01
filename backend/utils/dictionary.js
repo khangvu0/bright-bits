f816a259d353f93b0340b734c8afe1b3ca9ad050
Mhowey19 <michaelhowey5@gmail.com>
2025-10-01 13:42:15 -0400

    Fixed random input into dictionary issuese. Shows parsed data from the api using the random word

--- backend/utils/dictionary.js
+++ backend/utils/dictionary.js
@@ -1,24 +1,25 @@
 require("dotenv").config();
-const api = process.env.DICTIONARY_API_KEY;
-const dictionaryData = async (word) => {
+const DICTIONARY_API_KEY = process.env.DICTIONARY_API_KEY;
+const getWordDetails = async (word) => {
 	try {
-		const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${word}?key=${api}`;
+		const url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${word}?key=${DICTIONARY_API_KEY}`;
 		const response = await fetch(url);
 		const data = await response.json();
 
-		const items = data.slice(0, 3).map((item) => {
-			const word = item.hwi?.hw || "N/A";
+		const maxWordDetailObjects = 3;
+		const items = data.slice(0, maxWordDetailObjects).map((item) => {
+			const word = item.meta?.id.split(":")[0] || "N/A";
 			const definition = item.shortdef?.[0] || "No definition";
-			const pronounce = item.hwi?.prs?.[0]?.sound?.audio || "No audio";
-			const fl = item?.fl || "No ___"; //change later
-			return { word, definition, pronounce, fl };
+			const pronunciation = item.hwi?.prs?.[0]?.sound?.audio || "No audio";
+			const functionalLabel = item?.fl || "No ___"; //change later
+			return { word, definition, pronunciation, functionalLabel };
 		});
 		return items;
 	} catch (e) {
-		return e;
+		return "error pulling data";
 	}
 };
 
-console.log(dictionaryData());
+module.exports = getWordDetails;
 
-module.exports = dictionaryData;
+//work on name conventions
